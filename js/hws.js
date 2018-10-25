604aa997aa121d0f2a83e393fab80aafa38d55b9
235496139@qq.com <235496139@qq.com>
2018-10-25 10:51:07 +0800

    get random shape

--- js/hws.js
+++ js/hws.js
@@ -1,16 +1,29 @@
 
-// 获取所有.card元素 （这是一个数组）
+// 获取所有.card元素 （这是一个数列）
 const cardEl = document.querySelectorAll(".card");
 console.log(cardEl);
+
+//用Math.random()函数生成0~1之间的随机数与0.5比较，返回-1或1
+function randomsort(a, b) {
+	return Math.random()>.5 ? -1 : 1;
+}
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let data = [...arr,...arr];
+// arr.sort(randomsort);
+// data输出1-8随机数组
+data.sort(randomsort);
+console.info(data);
 
 // 用for循环，将所有取到的.card list集合 依次加入img
 for(var i=0; i<cardEl.length; i++) {
     card = cardEl[i];
+    // 给card添加子元素img
     const getImg = document.createElement("img");
-    getImg.src = "img/shape_8.png";
+    // 取到随机图片
+    getImg.src = "img/shape_" + data[i] + ".png";
+    // getImg.src = "img/shape_8.png";
     card.appendChild(getImg);
 }
-
 
 let img = document.getElementsByTagName('img');
 let deck = document.querySelector(".deck");
@@ -29,6 +42,8 @@ deck.addEventListener("click", function(event){
 
 
 
+
+
 // 为所有card添加点击事件 
 // let cardList = document.querySelectorAll('.card');
 //   for(var i=0; i<cardList.length; i++){
@@ -41,10 +56,7 @@ deck.addEventListener("click", function(event){
 // function changebg(deck){
 //     console.log(deck);
 
-// }
-// const getImg = document.createElement("img");
-// getImg.src = "img/shape_8.png";
-// cardEl.appendChild(getImg);  
+
 
 
 
