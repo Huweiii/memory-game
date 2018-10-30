b583223d2a59f7b635c4012c6548fde1a8e79710
235496139@qq.com <235496139@qq.com>
2018-10-30 17:07:45 +0800

    Ran all steps successfully

--- js/hws.js
+++ js/hws.js
@@ -1,30 +1,35 @@
+// 布置基本随机图形
 
-// 获取所有.card元素 （这是一个数列）
-const cardEl = document.querySelectorAll(".card");
-console.log(cardEl);
+function start(){
+    // 获取所有.card元素 （这是一个数列）
+    const cardEl = document.querySelectorAll(".card");
+    console.log(cardEl);
 
-//用Math.random()函数生成0~1之间的随机数与0.5比较，返回-1或1
-function randomsort(a, b) {
-	return Math.random()>.5 ? -1 : 1;
+    //用Math.random()函数生成0~1之间的随机数与0.5比较，返回-1或1
+    function randomsort(a, b) {
+        return Math.random()>.5 ? -1 : 1;
+    }
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+    let data = [...arr,...arr];
+    // arr.sort(randomsort);
+    // data输出1-8随机数组
+    data.sort(randomsort);
+    console.info(data);
+
+    // 用for循环，将所有取到的.card list集合 依次加入img
+    for(var i=0; i<cardEl.length; i++) {
+        card = cardEl[i];
+        // 给card添加子元素img
+        const getImg = document.createElement("img");
+        // 取到随机图片
+        getImg.src = "img/shape_" + data[i] + ".png";
+        // console.log(data[i]);
+        card.appendChild(getImg);
+    }
 }
-var arr = [1, 2, 3, 4, 5, 6, 7, 8];
-let data = [...arr,...arr];
-// arr.sort(randomsort);
-// data输出1-8随机数组
-data.sort(randomsort);
-console.info(data);
 
-// 用for循环，将所有取到的.card list集合 依次加入img
-for(var i=0; i<cardEl.length; i++) {
-    card = cardEl[i];
-    // 给card添加子元素img
-    const getImg = document.createElement("img");
-    // 取到随机图片
-    getImg.src = "img/shape_" + data[i] + ".png";
-    // console.log(data[i]);
-    card.appendChild(getImg);
-}
 
+start();
 
 
 // --------------------------------------------------
@@ -34,6 +39,7 @@ for(var i=0; i<cardEl.length; i++) {
 let deck = document.querySelector(".deck");
 let lastClick = null;
 let result = 0;
+let success = document.getElementsByClassName("success");
 
 // 点击事件
 deck.addEventListener("click", function(event){
@@ -61,8 +67,7 @@ deck.addEventListener("click", function(event){
                 console.log(result);
                
                 // 如果翻完，显示成功页面
-                let success = document.getElementsByClassName("success");
-                if (result == 8) {  
+                 if (result == 8) {  
                     console.log("All done")
                     success[0].style.display = "block";
                 };
@@ -88,7 +93,29 @@ deck.addEventListener("click", function(event){
     console.log(cardEl.outerHTML);
 });
 
-let success = document.getElementsByClassName("success");
+// 点击refresh按钮刷新页面
+let clickRefresh = document.querySelector(".refresh");
+let baseCard = document.getElementsByClassName("baseCard");
+let removeImg = document.querySelectorAll("img");
+    
+clickRefresh.addEventListener('click', function(){   
+    for(let j=0; j<baseCard.length; j++) {
+        removeImg[j].remove();
+        baseCard[j].firstElementChild.className = "card";
+    }
+    start();   
+});
 
+// 点击Again按钮刷新页面
+let clickAgain = document.querySelector(".again");
+    
+clickAgain.addEventListener('click', function(){   
+    for(let k=0; k<baseCard.length; k++) {
+        removeImg[k].remove();
+        baseCard[k].firstElementChild.className = "card";
+    }
+    success[0].style.display = "none";
+    start();   
+});
 
 
